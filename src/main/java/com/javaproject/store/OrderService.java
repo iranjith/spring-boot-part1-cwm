f77fe57d1d2703129c31165aac9fd1b41b7d53cd
Ranjith Kumar R G <theranjith@gmail.com>
2025-12-31 12:24:11 +0530

    dependency injection - constructor injection

---
+++ src/main/java/com/javaproject/store/OrderService.java
@@ -0,0 +1,11 @@
+package com.javaproject.store;
+
+public class OrderService {
+    private PaymentService paymentService;
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+    public void placeOrder(){
+        paymentService.processPayment(200);
+    }
+}
