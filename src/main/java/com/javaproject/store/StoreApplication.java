bf0666138fb2b916c683ecd04a9e8cd6fe381167
Ranjith Kumar R G <theranjith@gmail.com>
2026-01-06 22:57:09 +0530

    simplifed code with lombak and added data with builder pattern

--- src/main/java/com/javaproject/store/StoreApplication.java
+++ src/main/java/com/javaproject/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.javaproject.store;
 
+import com.javaproject.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +9,14 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
         public static void main(String[] args) {
-            ApplicationContext context= SpringApplication.run(StoreApplication.class, args);
+//            ApplicationContext context= SpringApplication.run(StoreApplication.class, args);
+           var user=User.builder()
+                   .name("John")
+                   .email("john@gmail.com")
+                   .password("1234")
+                   .build();
+
+
         }
 
 }
